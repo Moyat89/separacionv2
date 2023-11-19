bf16fc76ecbc1e6f372aaa1ad9edd5f3f5d756a5
Moyat89 <lagunamov@gmail.com>
2023-11-19 09:32:34 +0100

    final version

--- src/js/script_entrada.js
+++ src/js/script_entrada.js
@@ -10,11 +10,4 @@
         document.getElementById("body").style.alignItems = "center"; 
       };
       
-      function entrar() {
-        document.getElementById('boton').style.visibility = 'hidden';
-        document.getElementById('nav').style.visibility = 'visible';
-        document.getElementById('text').style.visibility = 'visible';
-        document.getElementById("desplegable").style.display = "block"; 
-        document.getElementById("desplegable").style.backgroundColor = "rgba(2, 5, 7, 0.8)";
-        document.getElementById("desplegable").style.border="2px solid black";
-      }
+checkin();
---
+++ src/js/script_entrar.js
@@ -0,0 +1,13 @@
+     function entrar() {
+        document.getElementById('boton').style.visibility = 'hidden';
+        document.getElementById('nav').style.visibility = 'visible';
+        document.getElementById('text').style.visibility = 'visible';
+        document.getElementById("desplegable").style.display = "block"; 
+        document.getElementById("desplegable").style.backgroundColor = "rgba(2, 5, 7, 0.8)";
+        document.getElementById("desplegable").style.border="2px solid black";
+      }
+
+      window.onload = function() {
+        var btn = document.getElementById("myButton");
+        btn.onclick = entrar;
+    }
